17052b074a2860b433bb7c800e9668f14a872eba
Ashish Kumar <akca38719@gmail.com>
2026-03-29 14:37:05 +0530

    Update ultrasonic_alert.ino

--- ultrasonic_alert.ino
+++ ultrasonic_alert.ino
@@ -1,11 +1,18 @@
 #define trigPin 9
 #define echoPin 10
 #define buzzer 8
-#define led1 7
-#define led2 13
+#define led1 7   // Red LED
+#define led2 13  // Green LED
 
 long duration;
 int distance;
+
+unsigned long startTime = 0;
+bool objectDetected = false;
+
+// blinking control
+unsigned long previousMillis = 0;
+bool ledState = false;
 
 void setup() {
   pinMode(trigPin, OUTPUT);
@@ -17,6 +24,7 @@ void setup() {
 }
 
 void loop() {
+  // Ultrasonic trigger
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
 
@@ -29,25 +37,47 @@ void loop() {
 
   Serial.println(distance);
 
-  if (distance < 10) {
-    digitalWrite(led2, LOW);
-    digitalWrite(led1, HIGH);
-    tone(buzzer, 1000); // continuous sound
-  }
-  else if (distance < 30) {
-    digitalWrite(led2, LOW);
-    digitalWrite(led1, HIGH);
-    tone(buzzer, 1000);
-    delay(200);
-    digitalWrite(led1, LOW);
-    noTone(buzzer);
-    delay(200);
-  }
-  else {
+  if (distance < 30) {
+
+    if (!objectDetected) {
+      startTime = millis();
+      objectDetected = true;
+    }
+
+    if (millis() - startTime >= 3000) {
+
+      // DANGER (<10 cm)
+      if (distance < 10) {
+        digitalWrite(led2, LOW);
+        digitalWrite(led1, HIGH);
+        tone(buzzer, 1000);
+      }
+
+      // ⚠️ WARNING (10–30 cm)
+      else {
+        digitalWrite(led2, LOW);
+
+        unsigned long currentMillis = millis();
+
+        if (currentMillis - previousMillis >= 200) {
+          previousMillis = currentMillis;
+          ledState = !ledState;
+          digitalWrite(led1, ledState);
+
+          if (ledState) {
+            tone(buzzer, 1000);
+          } else {
+            noTone(buzzer);
+          }
+        }
+      }
+    }
+
+  } else {
+    // SAFE
+    objectDetected = false;
     digitalWrite(led1, LOW);
     digitalWrite(led2, HIGH);
     noTone(buzzer);
   }
-
-  delay(100);
 }
